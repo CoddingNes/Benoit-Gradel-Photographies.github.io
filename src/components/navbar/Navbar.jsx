f61eb1988b0579ef471e5b265391bf9e7f54d04b
CoddingNes <axela.n@wanadoo.fr>
2023-11-20 11:44:55 +0100

    navbar style and legalNoctice content

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './navbar.scss';
 
 const Navbar = () => {
     return (
         <nav>
-            <NavLink to='/' className='navbar__link'>
-                Prestations
-            </NavLink>
-            <NavLink to='/particuliers/portraits' className='navbar__link'>
-                Portraits
-            </NavLink>
-            <NavLink to='/particuliers/mariage' className='navbar__link'>
-                Mariage
-            </NavLink>
-            <NavLink to='/entreprises' className='navbar__link'>
-                Entreprises
-            </NavLink>
-            <NavLink to='/gallerie' className='navbar__link'>
-                Gallerie
-            </NavLink>
-            <NavLink to='/espaceclient' className='navbar__link'>
-                Espace clients
-            </NavLink>
-            <NavLink to='/contact' className='navbar__link'>
-                Contact
-            </NavLink>
+            <ul className='navbar__list'>
+                <li>
+                    <NavLink to='/' className='navbar__link'>
+                        Prestations
+                    </NavLink>
+                </li>
+                <li className='navbar__link private-list'>
+                    <p className='navbar__link private-title'>
+                        Particuliers
+                    </p>
+
+                    <ul className='navbar__link dropdown-elements'>
+                        <li>
+                            <NavLink to='/particuliers/portraits' className='navbar__link dropdown-element1'>
+                                Portraits
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/particuliers/mariage' className='navbar__link dropdown-element2'>
+                                Mariage
+                            </NavLink>
+                        </li>
+                    </ul>
+                </li>
+                <li><NavLink to='/entreprises' className='navbar__link'>
+                    Entreprises
+                </NavLink>
+                </li>
+                <li><NavLink to='/gallerie' className='navbar__link'>
+                    Gallerie
+                </NavLink>
+                </li>
+                <li><NavLink to='/espaceclient' className='navbar__link'>
+                    Espace clients
+                </NavLink>
+                </li>
+                <li><NavLink to='/contact' className='navbar__link'>
+                    Contact
+                </NavLink>
+                </li>
+            </ul>
         </nav>
     );
 };
--- src/pages/legalNotices/LegalNotices.jsx
+++ src/pages/legalNotices/LegalNotices.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import './legalNotices.scss';
 
 const Notices = () => {
     return (
         <main>
             <Navbar />
-            Mentions Légales
+            <h2>Mentions légales</h2>
+            <p>Merci de lire attentivement les présentes modalités d’utilisation du présent site avant de le parcourir. En vous connectant sur ce site, vous acceptez sans réserve les présentes modalités.</p>
+            <h3>Entreprise individuelle</h3>
+            <p>Benoit GRADEL</p>
+            <p>50 Impasse des Chenilles</p>
+            <p>07130 TOULAUD</p>
+            <p>Activité artisanale immatriculée au répertoire des métions sous le numéro : 493 77 189 RM 07</p>
+            <h3>Editeur du site</h3>
+            <p>M Benoit GRADEL</p>
+            <p>50 Impasse des Chenilles</p>
+            <p>073130 TOULAUD</p>
+            <h3>Conditions d’utilisation</h3>
+            <p>Le site accessible par les url suivantes : www.benoit-gradel-photographies.fr/ est exploité dans le respect de la législation française. L’utilisation de ce site est régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société .
+                ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.</p>
+            <h3>Limitation de responsabilité</h3>
+            <p>Les informations contenues sur ce site sont aussi précises que possibles et le site est périodiquement remis à jour, mais peut toutefois contenir des inexactitudes, des omissions ou des lacunes. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email en décrivant le problème de la manière la plus précise possible (page posant problème, action déclenchante, type d’ordinateur et de navigateur utilisé, …).</p>
+            <p>Tout contenu téléchargé se fait aux risques et périls de l’utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d’un quelconque dommage subi par l’ordinateur de l’utilisateur ou d’une quelconque perte de données consécutives au téléchargement.
+            </p>
+            <p>Les photos sont non contractuelles.
+            </p>
+            <p>Les liens hypertextes mis en place dans le cadre du présent site internet en direction d’autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de .
+            </p>
+
+
+            <h3>Litiges
+            </h3>
+            <p>Les présentes conditions sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l’interprétation ou de l’exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société . La langue de référence, pour le règlement de contentieux éventuels, est le français.
+            </p>
+
+            <h3>Propriété intellectuelle
+            </h3>
+            <p>Tout le contenu du présent site, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l’exception des marques, logos ou contenus appartenant à d’autres sociétés partenaires ou auteurs.
+                <br />Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l’accord exprès par écrit de M Benoit GRADEL. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.3335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre. Benoit Gradel Photographies est identiquement propriétaire des « droits des producteurs de bases de données » visés au Livre III, Titre IV, du Code de la Propriété Intellectuelle (loi n° 98-536 du 1er juillet 1998) relative aux droits d’auteur et aux bases de données.
+            </p>
+
+            <h3>Hébergeur
+            </h3>
+            <p>société OVH<br />www.ovh.com</p>
+
+            <h2>Conditions de service
+            </h2>
+            <p>Ce site est proposé en langages HTML5 et CSS3, pour un meilleur confort d’utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Safari, Firefox, Chrome,…
+            </p>
+            <h3>Informations et exclusion</h3>
+            <p>met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L’internaute devra donc s’assurer de l’exactitude des informations auprès de , et signaler toutes modifications du site qu’il jugerait utile. n’est en aucun cas responsable de l’utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.</p>
+
+
+            <h3>Liens hypertextes</h3>
+            <p>Les sites internet de peuvent offrir des liens vers d’autres sites internet ou d’autres ressources disponibles sur Internet.</p>
+            <p>Les utilisateurs, les abonnés et les visiteurs des sites internet de ne peuvent mettre en place un hyperlien en direction de ce site sans l’autorisation expresse et préalable de Benoit Gradel Photographies.</p>
+            <p>Dans l’hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un site internet, il lui appartiendra d’adresser un
+                <a mailto="postmaster@benoit-gradel-photographies.fr" className="link__email">email</a>
+                afin de formuler sa demande de mise en place d’un hyperlien. Le webmaster se réserve le droit d’accepter ou de refuser un hyperlien sans avoir à en justifier sa décision.
+            </p>
+
+            <h3>Responsabilité</h3>
+            <p>Aucune autre garantie n’est accordée au client, auquel incombe l’obligation de formuler clairement ses besoins et le devoir de s’informer. Si des informations fournies par Benoit Gradel Photographies apparaissent inexactes, il appartiendra au client de procéder lui-même à toutes vérifications de la cohérence ou de la vraisemblance des résultats obtenus. Benoit Gradel Photographies ne sera en aucune façon responsable vis à vis des tiers de l’utilisation par le client des informations ou de leur absence contenues dans ses produits y compris un de ses sites Internet.</p>
+
+            <h3>Contactez-nous</h3>
+            <p>Benoit Gradel Photographies est à votre disposition pour tous vos commentaires ou suggestions. Vous pouvez nous écrire en français par courrier électronique à : postmaster@benoit-gradel-photographies.fr</p>
         </main>
     );
 };
